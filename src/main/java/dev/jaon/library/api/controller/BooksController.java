3f98baa5fd830281889384c10345c9f69d209a26
Jean Nunes <jaon@itriad.org.br>
2021-02-18 11:46:54 -0400

    Habilitando CORS

--- src/main/java/dev/jaon/library/api/controller/BooksController.java
+++ src/main/java/dev/jaon/library/api/controller/BooksController.java
@@ -3,6 +3,7 @@ package dev.jaon.library.api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import dev.jaon.library.api.model.Book;
 import dev.jaon.library.api.service.BooksService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/books")
 public class BooksController {

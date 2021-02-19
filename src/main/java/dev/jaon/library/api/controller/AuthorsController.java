697324afe8e06454c65c19be2a5104d334face9d
Jean Nunes <jaon@itriad.org.br>
2021-02-19 14:57:36 -0400

    Gerenciamento de autores

--- src/main/java/dev/jaon/library/api/controller/AuthorsController.java
+++ src/main/java/dev/jaon/library/api/controller/AuthorsController.java
@@ -1,12 +1,20 @@
 package dev.jaon.library.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.jaon.library.api.model.Author;
+import dev.jaon.library.api.model.Book;
 import dev.jaon.library.api.service.AuthorsService;
 
 @CrossOrigin
@@ -20,4 +28,31 @@ public class AuthorsController {
 	public Author[] index() {
 		return authorsService.findAll();
 	}
+
+	@GetMapping("/{id}")
+	public Author getAuthor(@PathVariable(value = "id") Long id) {
+		return authorsService.findOne(id);
+	}
+
+	@PostMapping
+	public Author createAuthor(@RequestBody Author author) {
+		return authorsService.create(author);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Author> updateAuthor(@RequestBody Author author) {
+		return authorsService.update(author);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Object> deleteAuthor(@PathVariable(value = "id") Long id) {
+		authorsService.delete(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
+	@GetMapping("/{id}/books")
+	public Book[] getAuthorBooks(@PathVariable(value = "id") Long id) {
+		return authorsService.getAuthorBooks(id);
+	}
+
 }
--- src/main/java/dev/jaon/library/api/service/AuthorsService.java
+++ src/main/java/dev/jaon/library/api/service/AuthorsService.java
@@ -1,9 +1,13 @@
 package dev.jaon.library.api.service;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import dev.jaon.library.api.model.Author;
+import dev.jaon.library.api.model.Book;
 
 @Service
 public class AuthorsService {
@@ -13,5 +17,36 @@ public class AuthorsService {
 		RestTemplate restTemplate = new RestTemplate();
 		Author[] authors = restTemplate.getForObject(apiUrl, Author[].class);
 		return authors;
+	}
+
+	public Author findOne(Long id) {
+		RestTemplate restTemplate = new RestTemplate();
+		Author author = restTemplate.getForObject(apiUrl + "/" + id, Author.class);
+		return author;
+	}
+
+	public Author create(Author author) {
+		RestTemplate restTemplate = new RestTemplate();
+		Author createdAuthor = restTemplate.postForObject(apiUrl, author, Author.class);
+		return createdAuthor;
+	}
+
+	public ResponseEntity<Author> update(Author author) {
+		RestTemplate restTemplate = new RestTemplate();
+		HttpEntity<Author> requestUpdate = new HttpEntity<>(author);
+		ResponseEntity<Author> response = restTemplate.exchange(apiUrl + "/" + author.getId(), HttpMethod.PUT,
+				requestUpdate, Author.class);
+		return response;
+	}
+
+	public void delete(Long id) {
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.delete(apiUrl + "/" + id);
+	}
+
+	public Book[] getAuthorBooks(Long id) {
+		RestTemplate restTemplate = new RestTemplate();
+		Book[] books = restTemplate.getForObject(apiUrl + "/" + id + "/books", Book[].class);
+		return books;
 	}
 }
